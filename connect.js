cf9bd90fd661885d96ab6e79719a5fdc5290bc23
Ev Bogue <ev@evbogue.com>
2025-03-28 11:58:59 -0500

    send messages and blobs to dovepub

--- connect.js
+++ connect.js
@@ -3,6 +3,20 @@ import { makeRoom } from './gossip.js'
 
 const pubkeys = new Set()
 
+const ws = new WebSocket('wss://pub.wiredove.net/')
+ws.onopen = async (e) => {
+  console.log('OPEN')
+  const log = await bogbot.query()
+  for (const msg of log) {
+    ws.send(msg.sig)
+    ws.send(msg.text)
+    ws.send(msg.hash)
+    ws.send(msg.opened.substring(13))
+  }
+}
+
+
+
 export const connect = async () => {
   const log = await bogbot.getOpenedLog()
   const pubkey = await bogbot.pubkey()
@@ -17,4 +31,5 @@ export const connect = async () => {
       await makeRoom(pubkey, pubkeys)
     })
   }
+
 }
