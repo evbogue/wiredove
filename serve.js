bb330c597784a4444eacd63b6f82505ccab3d08f
Ev Bogue <ev@evbogue.com>
2025-04-04 14:45:45 -0500

    set serve version to avoid implicit warning

--- serve.js
+++ serve.js
@@ -1,4 +1,4 @@
-import { serveDir } from 'https://deno.land/std/http/file_server.ts'
+import { serveDir } from 'https://deno.land/std@0.224.0/http/file_server.ts'
 
 Deno.serve((r) => {
   return serveDir(r, {quiet: 'True'})
