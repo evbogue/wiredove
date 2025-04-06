41fc4ff3377df5e7f0568377a46ed2e99390dcf7
Ev Bogue <ev@evbogue.com>
2025-04-06 14:52:00 -0500

    request latest messages from pubkeys

--- connect.js
+++ connect.js
@@ -18,23 +18,25 @@ const startWs = async (pub) => {
   ws.onopen = async () => {
     console.log('OPEN')
     pubs.add(ws)
-    const log = await bogbot.query()
-    if (log) { 
-      for (const msg of log) {
-        ws.send(msg.sig)
-        ws.send(msg.text)
-        //ws.send(msg.hash)
-        //ws.send(msg.opened.substring(13))
-      }
+    const p = await bogbot.getPubkeys()
+    for (const pub of p) {
+      ws.send(pub)
     }
+    //const log = await bogbot.query()
+    //if (log) { 
+    //  for (const msg of log) {
+    //    ws.send(msg.sig)
+    //    ws.send(msg.text)
+    //    //ws.send(msg.hash)
+    //    //ws.send(msg.opened.substring(13))
+    //  }
+    //}
   }
 
   ws.onmessage = async (m) => {
     await render.shouldWe(m.data)
     await bogbot.add(m.data)
     await render.blob(m.data)
-
-    console.log(m.data)
   }
 
   ws.onclose = async () => {
@@ -44,13 +46,13 @@ const startWs = async (pub) => {
 }
 
 export const connect = async () => {
-  await startWs('wss://pub.wiredove.net')
-  //await startWs('ws://localhost:9000')
+  //await startWs('wss://pub.wiredove.net')
+  await startWs('ws://localhost:9000')
   const pubkey = await bogbot.pubkey()
   const pubkeys = await bogbot.getPubkeys()
   if (pubkeys) {
     for (const p of pubkeys) {
-      await makeRoom(pubkey, pubkeys)    
+      await makeRoom(p, pubkeys)    
     }
   }
 }
