34a45389255db7d50926e8be9d3200472d8c63f5
Ev Bogue <ev@evbogue.com>
2025-04-06 11:54:56 -0500

    use new functions

--- gossip.js
+++ gossip.js
@@ -1,21 +1,11 @@
 import { bogbot } from 'bogbot'
-import { joinRoom } from 'https://esm.sh/gh/evbogue/bog5@38ac1c121f/lib/trystero-torrent.min.js'
+import { joinRoom } from './trystero-torrent.min.js'
 import { render } from './render.js'
 import { send } from './connect.js'
 
 export const rooms = new Map()
 
 const queue = new Set()
-
-const getLatest = async (pubkey) => {
-  const openedlog = await bogbot.getOpenedLog()
-  const reversedlog = openedlog.slice().reverse()
-  for (const msg of reversedlog) {
-    if (msg.author == pubkey) {
-      return msg.sig
-    }
-  } 
-}
 
 export const gossip = async (hash) => {
   queue.add(hash)
@@ -54,7 +44,7 @@ export const blast = async (pubkey, blob) => {
   }
 }
 
-export const makeRoom = async (pubkey, pubkeys) => {
+export const makeRoom = async (pubkey) => {
   const get = rooms.get(pubkey)
 
   if (!get) {
@@ -69,15 +59,12 @@ export const makeRoom = async (pubkey, pubkeys) => {
       //console.log(`Received: ${hash}`)
       const get = await bogbot.get(hash)
       if (get) { sendBlob(get, id)}
-      if (pubkeys && pubkeys.has(hash)) {
-        const latest = await getLatest(hash)
-        if (latest) { room.sendBlob(latest)}
-      }
+      const latest = await bogbot.getLatest(hash)
+      if (latest) { room.sendBlob(latest.sig)}
     }) 
 
     onBlob(async (blob, id) => {
       //console.log(`Received: ${blob}`)
-      //const hash = await bogbot.make(blob)
       await render.shouldWe(blob)
       await bogbot.add(blob)
       await render.blob(blob)
@@ -85,19 +72,15 @@ export const makeRoom = async (pubkey, pubkeys) => {
 
     room.onPeerJoin(async (id) => {
       console.log(id + ' joined the room ' + pubkey)
-      const latest = await getLatest(pubkey)
-      if (latest) {
-        setTimeout(() => {room.sendBlob(latest)}, 1000)
+      room.sendHash(pubkey)
+      const pubkeys = await bogbot.getPubkeys()
+      for (const key of pubkeys) {
+        room.sendHash(key)
+        const latest = await bogbot.getLatest(key)
+        if (latest) {
+          room.sendBlob(latest.sig)
+        }
       }
-      //if (pubkeys && pubkeys.size > 0) {
-      //  pubkeys.forEach(async (key) => {
-      //    room.sendHash(key)
-      //    const latest = await getLatest(key)
-      //    if (latest) {
-      //      room.sendBlob(latest)
-      //    }
-      //  })
-      //}
     })
 
     room.onPeerLeave(id => {
