0180467988f4225c3ca29a93ca11ab922618cec5
Everett Bogue <ev@evbogue.com>
2025-06-18 12:59:25 -0500

    detect hash after so adder does not throw errs

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ import { route } from './route.js'
 import { connect } from './connect.js'
 import { navbar } from './navbar.js'
 
-if (!window.location.hash) { window.location = '#' }
-
 await bogbot.start('wiredovedbversion1')
-
 document.body.appendChild(await navbar())
-
 await route()
 await connect()
 
+if (!window.location.hash) { window.location = '#' } 
