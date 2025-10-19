93efa6a3ea010bdb143aba62982f4801720ce6a0
Everett Bogue <ev@evbogue.com>
2025-10-19 13:54:24 -0500

    use apds.aproto.com for websocket server

--- connect.js
+++ connect.js
@@ -5,6 +5,8 @@ import { makeWs} from './websocket.js'
 await apds.start('wiredovedbversion1')
 
 export const connect = async () => {
-  await makeWs('wss://pub.wiredove.net/')
+  //await makeWs('ws://localhost:9000')
+  await makeWs('wss://apds.anproto.com/')
+  //await makeWs('wss://pub.wiredove.net/')
   await makeRoom('wiredovev1')    
 }
