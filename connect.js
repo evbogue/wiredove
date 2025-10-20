9e8efbce1a39f89a7e785b952c9ba9a8cbd3b49f
Everett Bogue <ev@evbogue.com>
2025-10-20 18:41:55 -0500

    deno deploy toasts local cache so switch back to the vps apds

--- connect.js
+++ connect.js
@@ -6,7 +6,7 @@ await apds.start('wiredovedbversion1')
 
 export const connect = async () => {
   //await makeWs('ws://localhost:9000')
-  await makeWs('wss://apds.anproto.com/')
-  //await makeWs('wss://pub.wiredove.net/')
+  //await makeWs('wss://apds.anproto.com/')
+  await makeWs('wss://pub.wiredove.net/')
   await makeRoom('wiredovev1')    
 }
