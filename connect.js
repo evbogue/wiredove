90bb6f3d228c1264cc81eaa097f1bdd8e159eaee
Everett Bogue <ev@evbogue.com>
2026-01-21 16:17:41 -0600

    use pub.wiredove.net

--- connect.js
+++ connect.js
@@ -6,10 +6,10 @@ import { send } from './send.js'
 await apds.start('wiredovedbversion1')
 
 export const connect = async () => {
-  await makeWs('ws://localhost:9000')
+  //await makeWs('ws://localhost:9000')
   //await makeWs('wss://apds.anproto.com/')
   makeWs('wss://pub.wiredove.net/')
-  await startHttpGossip('http://localhost:9000')
+  await startHttpGossip('https://pub.wiredove.net')
   makeRoom('wiredovev1')
   send('evSFOKnXaF9ZWSsff8bVfXP6+XnGZUj8XNp6bca590k=')
 }
